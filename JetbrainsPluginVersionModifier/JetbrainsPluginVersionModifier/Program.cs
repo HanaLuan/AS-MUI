d82bc21752e3a0aaf64caf6b7379ae1293d1ffb2
鸞瑤綾舞 <105547626+luanyaolingwu@users.noreply.github.com>
2025-04-18 09:40:13 +0800

    None

--- JetbrainsPluginVersionModifier/JetbrainsPluginVersionModifier/Program.cs
+++ JetbrainsPluginVersionModifier/JetbrainsPluginVersionModifier/Program.cs
@@ -30,76 +30,97 @@ namespace JetbrainsPluginVersionModifier {
 			}
 		}
 
-		static async Task ModifyAsync (string path, string sinceVersion, bool isWebApi, bool isIndent) {
+		static async Task ModifyAsync(string path, string sinceVersion, bool isWebApi, bool isIndent) {
 			try {
 				CommandLineWriter.IsWebApi = isWebApi;
 				if (isIndent) {
 					CommandLineWriter.Formatting = Newtonsoft.Json.Formatting.Indented;
 				}
 				string[] paths;
-				if (Directory.Exists (path)) {
-					paths = Directory.GetFiles (path, "*.jar", SearchOption.AllDirectories);
+				if (Directory.Exists(path)) {
+					paths = Directory.GetFiles(path, "*.jar", SearchOption.AllDirectories);
 				} else {
 					paths = [path];
 				}
-				var jsonArray = new JArray ();
+				var jsonArray = new JArray();
 				for (var i = 0; i < paths.Length; i++) {
-					var fileInfo = new FileInfo (paths[i]);
+					var fileInfo = new FileInfo(paths[i]);
 					string oldSinceVersion;
 					string newSinceVersion;
-					using (var fileStream = fileInfo.Open (FileMode.Open, FileAccess.ReadWrite)) {
-						using var zipArchive = new ZipArchive (fileStream, ZipArchiveMode.Update);
+					string originVersion = "";
+					string newName = "";
+					using (var fileStream = fileInfo.Open(FileMode.Open, FileAccess.ReadWrite)) {
+						using var zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Update);
 						var xmlEntryPath = "META-INF/plugin.xml";
-						var xmlEntry = zipArchive.GetEntry (xmlEntryPath);
+						var xmlEntry = zipArchive.GetEntry(xmlEntryPath);
 						if (xmlEntry == null) {
-							throw new FileNotFoundException ($"jar内未找到 {xmlEntryPath} 文件");
+							throw new FileNotFoundException($"jar内未找到 {xmlEntryPath} 文件");
 						}
-						var xmlDocument = new XmlDocument ();
-						using (var xmlStream = xmlEntry.Open ()) {
-							using var xmlStreamReader = new StreamReader (xmlStream);
-							var xmlText = await xmlStreamReader.ReadToEndAsync ();
-							xmlDocument.LoadXml (xmlText);
-							var ideaVersionNodePath = "idea-version";
-							var ideaVersionNode = xmlDocument.DocumentElement.SelectSingleNode (ideaVersionNodePath);
+						var xmlDocument = new XmlDocument();
+						using (var xmlStream = xmlEntry.Open()) {
+							using var xmlStreamReader = new StreamReader(xmlStream);
+							var xmlText = await xmlStreamReader.ReadToEndAsync();
+							xmlDocument.LoadXml(xmlText);
+							var root = xmlDocument.DocumentElement;
+							// Get and modify <idea-version>
+							var ideaVersionNode = root.SelectSingleNode("idea-version");
 							if (ideaVersionNode == null) {
-								throw new Exception ($"{xmlEntryPath} 内未找到 {ideaVersionNode} 节点");
+								throw new Exception($"{xmlEntryPath} 内未找到 idea-version 节点");
 							}
-							var versionAttributePath = "since-build";
-							var versionAttribute = ideaVersionNode.Attributes[versionAttributePath];
+							var versionAttribute = ideaVersionNode.Attributes["since-build"];
+							var untilBuildAttribute = ideaVersionNode.Attributes["until-build"];
 							if (versionAttribute == null) {
-								throw new Exception ($"{ideaVersionNode} 节点没有 {versionAttributePath} 属性");
+								throw new Exception($"idea-version 节点没有 since-build 属性");
+							}
+							if (untilBuildAttribute == null) {
+								untilBuildAttribute = xmlDocument.CreateAttribute("until-build");
+								ideaVersionNode.Attributes.Append(untilBuildAttribute);
 							}
 							oldSinceVersion = versionAttribute.Value;
-							if (!string.IsNullOrEmpty (sinceVersion)) {
+							untilBuildAttribute.Value = "1012.2407.50";
+							if (!string.IsNullOrEmpty(sinceVersion)) {
 								newSinceVersion = sinceVersion;
 							} else {
-								var versions = versionAttribute.Value.Split ('.');
+								var versions = versionAttribute.Value.Split('.');
 								for (var n = 1; n < versions.Length; n++) {
 									versions[n] = "0";
 								}
-								newSinceVersion = string.Join ('.', versions);
+								newSinceVersion = string.Join('.', versions);
 							}
 							versionAttribute.Value = newSinceVersion;
+							// Get and modify <version>
+							var versionNode = root.SelectSingleNode("version");
+							if (versionNode != null) {
+								originVersion = versionNode.InnerText;
+							}
+							// Modify <name>
+							var nameNode = root.SelectSingleNode("name");
+							if (nameNode != null) {
+								newName = $"{nameNode.InnerText} [Origin: ideaIU {originVersion}]";
+								nameNode.InnerText = newName;
+							}
 						}
-						xmlEntry.Delete ();
-						xmlEntry = zipArchive.CreateEntry (xmlEntryPath, CompressionLevel.Optimal);
-						using var xmlStreamWriter = new StreamWriter (xmlEntry.Open ());
-						xmlDocument.Save (xmlStreamWriter);
+						xmlEntry.Delete();
+						xmlEntry = zipArchive.CreateEntry(xmlEntryPath, CompressionLevel.Optimal);
+						using var xmlStreamWriter = new StreamWriter(xmlEntry.Open());
+						xmlDocument.Save(xmlStreamWriter);
 					}
-					using var sha256 = SHA256.Create ();
-					using var stream = fileInfo.OpenRead ();
-					var hash = Convert.ToHexString (sha256.ComputeHash (stream)).ToLowerInvariant ();
-					CommandLineWriter.WriteLine ($"Since版本：{oldSinceVersion} 修改后：{newSinceVersion} Sha256：{hash} 已修改原文件：{fileInfo}");
-					jsonArray.Add (new JObject () {
-							{ "sinceVersion", oldSinceVersion },
-							{ "newSinceVersion", newSinceVersion },
-							{ "sha256", hash },
-							{ "path", fileInfo.FullName }
-						});
+					using var sha256 = SHA256.Create();
+					using var stream = fileInfo.OpenRead();
+					var hash = Convert.ToHexString(sha256.ComputeHash(stream)).ToLowerInvariant();
+					CommandLineWriter.WriteLine($"Since版本：{oldSinceVersion} 修改后：{newSinceVersion} Sha256：{hash} 已修改原文件：{fileInfo}");
+					jsonArray.Add(new JObject {
+						{ "sinceVersion", oldSinceVersion },
+						{ "newSinceVersion", newSinceVersion },
+						{ "sha256", hash },
+						{ "newName", newName },
+						{ "path", fileInfo.FullName }
+					});
 				}
-				CommandLineWriter.SetData (() => jsonArray);
+
+				CommandLineWriter.SetData(() => jsonArray);
 			} catch (Exception exception) {
-				CommandLineWriter.SetException (exception);
+				CommandLineWriter.SetException(exception);
 			}
 		}
 
