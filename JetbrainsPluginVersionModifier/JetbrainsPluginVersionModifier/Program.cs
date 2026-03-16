afd77b56ba8832baa0ac6f41f981eeaf21d1d8e2
鸞瑤綾舞 <105547626+luanyaolingwu@users.noreply.github.com>
2026-03-17 05:02:40 +0800

    None

--- JetbrainsPluginVersionModifier/JetbrainsPluginVersionModifier/Program.cs
+++ JetbrainsPluginVersionModifier/JetbrainsPluginVersionModifier/Program.cs
@@ -49,139 +49,135 @@ namespace JetbrainsPluginVersionModifier
                 CommandLineWriter.IsWebApi = isWebApi;
 
                 if (isIndent)
-                {
                     CommandLineWriter.Formatting = Newtonsoft.Json.Formatting.Indented;
-                }
 
                 string[] paths;
 
                 if (Directory.Exists(path))
-                {
                     paths = Directory.GetFiles(path, "*.jar", SearchOption.AllDirectories);
-                }
                 else
-                {
                     paths = new[] { path };
-                }
 
                 var jsonArray = new JArray();
 
-                for (var i = 0; i < paths.Length; i++)
+                foreach (var jarPath in paths)
                 {
-                    var fileInfo = new FileInfo(paths[i]);
+                    var fileInfo = new FileInfo(jarPath);
 
-                    string oldSinceVersion;
-                    string newSinceVersion;
+                    string oldSinceVersion = "";
+                    string newSinceVersion = "";
                     string originVersion = "";
                     string newName = "";
 
-                    using (var fileStream = fileInfo.Open(FileMode.Open, FileAccess.ReadWrite))
+                    byte[] newJarBytes;
+
+                    using (var originalStream = fileInfo.OpenRead())
+                    using (var originalZip = new ZipArchive(originalStream, ZipArchiveMode.Read))
+                    using (var memory = new MemoryStream())
                     {
-                        using var zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Update);
-
-                        // 删除 jar index
-                        zipArchive.GetEntry("__index__")?.Delete();
-
-                        // 删除签名文件
-                        foreach (var entry in zipArchive.Entries
-                                     .Where(e => e.FullName.StartsWith("META-INF/", StringComparison.OrdinalIgnoreCase) &&
-                                                 (e.FullName.EndsWith(".SF", StringComparison.OrdinalIgnoreCase) ||
-                                                  e.FullName.EndsWith(".RSA", StringComparison.OrdinalIgnoreCase) ||
-                                                  e.FullName.EndsWith(".DSA", StringComparison.OrdinalIgnoreCase)))
-                                     .ToList())
-                        {
-                            entry.Delete();
-                        }
-
-                        var xmlEntryPath = "META-INF/plugin.xml";
-                        var xmlEntry = zipArchive.GetEntry(xmlEntryPath);
-
-                        if (xmlEntry == null)
-                        {
-                            throw new FileNotFoundException($"jar内未找到 {xmlEntryPath} 文件");
-                        }
-
                         var xmlDocument = new XmlDocument();
 
-                        using (var xmlStream = xmlEntry.Open())
-                        using (var xmlStreamReader = new StreamReader(xmlStream))
+                        var pluginEntry = originalZip.GetEntry("META-INF/plugin.xml");
+
+                        if (pluginEntry == null)
+                            throw new FileNotFoundException("jar内未找到 META-INF/plugin.xml 文件");
+
+                        using (var xmlStream = pluginEntry.Open())
+                        using (var reader = new StreamReader(xmlStream))
                         {
-                            var xmlText = await xmlStreamReader.ReadToEndAsync();
-
+                            var xmlText = await reader.ReadToEndAsync();
                             xmlDocument.LoadXml(xmlText);
-
-                            var root = xmlDocument.DocumentElement;
-
-                            var ideaVersionNode = root?.SelectSingleNode("idea-version");
-
-                            if (ideaVersionNode == null)
-                            {
-                                throw new Exception($"{xmlEntryPath} 内未找到 idea-version 节点");
-                            }
-
-                            var versionAttribute = ideaVersionNode.Attributes?["since-build"];
-                            var untilBuildAttribute = ideaVersionNode.Attributes?["until-build"];
-
-                            if (versionAttribute == null)
-                            {
-                                throw new Exception("idea-version 节点没有 since-build 属性");
-                            }
-
-                            if (untilBuildAttribute == null)
-                            {
-                                untilBuildAttribute = xmlDocument.CreateAttribute("until-build");
-                                ideaVersionNode.Attributes.Append(untilBuildAttribute);
-                            }
-
-                            oldSinceVersion = versionAttribute.Value;
-
-                            untilBuildAttribute.Value = "1012.2407.50";
-
-                            if (!string.IsNullOrEmpty(sinceVersion))
-                            {
-                                newSinceVersion = sinceVersion;
-                            }
-                            else
-                            {
-                                var versions = versionAttribute.Value.Split('.');
-
-                                for (var n = 1; n < versions.Length; n++)
-                                {
-                                    versions[n] = "0";
-                                }
-
-                                newSinceVersion = string.Join('.', versions);
-                            }
-
-                            versionAttribute.Value = newSinceVersion;
-
-                            var versionNode = root.SelectSingleNode("version");
-
-                            if (versionNode != null)
-                            {
-                                originVersion = versionNode.InnerText;
-                            }
-
-                            var nameNode = root.SelectSingleNode("name");
-
-                            if (nameNode != null)
-                            {
-                                newName = $"{nameNode.InnerText} [Origin: ideaIU {originVersion}]";
-                                nameNode.InnerText = newName;
-                            }
                         }
 
-                        xmlEntry.Delete();
+                        var root = xmlDocument.DocumentElement;
 
-                        xmlEntry = zipArchive.CreateEntry(xmlEntryPath, CompressionLevel.Optimal);
+                        var ideaVersionNode = root?.SelectSingleNode("idea-version");
 
-                        using var xmlStreamWriter = new StreamWriter(xmlEntry.Open());
+                        if (ideaVersionNode == null)
+                            throw new Exception("plugin.xml 内未找到 idea-version 节点");
 
-                        xmlDocument.Save(xmlStreamWriter);
+                        var versionAttribute = ideaVersionNode.Attributes?["since-build"];
+                        var untilBuildAttribute = ideaVersionNode.Attributes?["until-build"];
+
+                        if (versionAttribute == null)
+                            throw new Exception("idea-version 节点没有 since-build 属性");
+
+                        if (untilBuildAttribute == null)
+                        {
+                            untilBuildAttribute = xmlDocument.CreateAttribute("until-build");
+                            ideaVersionNode.Attributes.Append(untilBuildAttribute);
+                        }
+
+                        oldSinceVersion = versionAttribute.Value;
+
+                        untilBuildAttribute.Value = "1012.2407.50";
+
+                        if (!string.IsNullOrEmpty(sinceVersion))
+                        {
+                            newSinceVersion = sinceVersion;
+                        }
+                        else
+                        {
+                            var versions = versionAttribute.Value.Split('.');
+
+                            for (var n = 1; n < versions.Length; n++)
+                                versions[n] = "0";
+
+                            newSinceVersion = string.Join('.', versions);
+                        }
+
+                        versionAttribute.Value = newSinceVersion;
+
+                        var versionNode = root.SelectSingleNode("version");
+
+                        if (versionNode != null)
+                            originVersion = versionNode.InnerText;
+
+                        var nameNode = root.SelectSingleNode("name");
+
+                        if (nameNode != null)
+                        {
+                            newName = $"{nameNode.InnerText} [Origin: ideaIU {originVersion}]";
+                            nameNode.InnerText = newName;
+                        }
+
+                        using (var newZip = new ZipArchive(memory, ZipArchiveMode.Create, true))
+                        {
+                            foreach (var entry in originalZip.Entries)
+                            {
+                                if (entry.FullName == "__index__")
+                                    continue;
+
+                                if (entry.FullName.Equals("META-INF/plugin.xml", StringComparison.OrdinalIgnoreCase))
+                                    continue;
+
+                                if (entry.FullName.StartsWith("META-INF/", StringComparison.OrdinalIgnoreCase) &&
+                                   (entry.FullName.EndsWith(".SF", StringComparison.OrdinalIgnoreCase) ||
+                                    entry.FullName.EndsWith(".RSA", StringComparison.OrdinalIgnoreCase) ||
+                                    entry.FullName.EndsWith(".DSA", StringComparison.OrdinalIgnoreCase)))
+                                    continue;
+
+                                var newEntry = newZip.CreateEntry(entry.FullName, CompressionLevel.Optimal);
+
+                                using var inStream = entry.Open();
+                                using var outStream = newEntry.Open();
+
+                                await inStream.CopyToAsync(outStream);
+                            }
+
+                            var newPluginEntry = newZip.CreateEntry("META-INF/plugin.xml", CompressionLevel.Optimal);
+
+                            using var writer = new StreamWriter(newPluginEntry.Open());
+
+                            xmlDocument.Save(writer);
+                        }
+
+                        newJarBytes = memory.ToArray();
                     }
 
-                    using var sha256 = SHA256.Create();
+                    await File.WriteAllBytesAsync(fileInfo.FullName, newJarBytes);
 
+                    using var sha256 = SHA256.Create();
                     using var stream = fileInfo.OpenRead();
 
                     var hash = Convert.ToHexString(sha256.ComputeHash(stream)).ToLowerInvariant();
